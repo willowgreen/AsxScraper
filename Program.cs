77a48fec6ee324a942f860c1a588514366d0faf1
Nicholas Schlobohm <nschlobohm@willowgreengroup.com.au>
2019-09-02 22:47:36 +1000

    Extremely basic functionality

--- Program.cs
+++ Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
 
 namespace AsxDataPuller
 {
@@ -6,7 +13,29 @@ namespace AsxDataPuller
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Title = "AsxDataPuller";
+
+            Console.Write("Enter a 3-character ASX ticker code to display the price of: ");
+            var input = Console.ReadLine();
+
+            if (input.Length != 3)
+            {
+                Console.Write("Error: the ticker code you enter must be 3 characters. Press any key to exit...");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+
+            var details = JObject.Parse(new WebClient().DownloadString("https://www.asx.com.au/asx/1/share/" + input.ToUpper()));
+
+            Console.WriteLine(string.Concat(details["code"], ": " + details["last_price"]));
+
+            // this works too
+            // Console.WriteLine(new WebClient().DownloadString("https://www.asx.com.au/asx/research/ASXListedCompanies.csv"));
+
+            Console.Write("Press any key to exit...");
+            Console.ReadKey();
+            Environment.Exit(0);
         }
     }
 }
